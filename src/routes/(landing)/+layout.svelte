d28639d6d0d09d0a6c81ded5bb3b3e5abfb71771
ercel <ercel.sotto@gmail.com>
2026-04-03 23:25:44 +1300

    Temp commit on the landing page.
    Got the skeleton up but still working on the main content of the landing
    page. Trying to get used to tailwindcss and sveltekit at the moment.

--- src/routes/(landing)/+layout.svelte
+++ src/routes/(landing)/+layout.svelte
@@ -2,6 +2,25 @@
 	let { children } = $props();
 </script>
 
-<div class="min-h-screen bg-background">
-	{@render children()}
+<div class="flex min-h-screen flex-col bg-background">
+	<header class="surface-container-high flex items-center justify-between px-8 py-8">
+		<h1 class="text-lg font-bold">BudgetBook</h1>
+		<nav class="flex items-center gap-4">
+			<a class="text-sm uppercase" href="/features">Features</a>
+			<a class="text-sm uppercase" href="/pricing">Pricing</a>
+			<a class="text-sm uppercase" href="/signup">Sign Up</a>
+		</nav>
+	</header>
+
+	<main class="surface-container-low primary-background flex-1">
+		{@render children()}
+	</main>
+	<footer class="items-right surface-container-lowest flex justify-between px-8 py-12">
+		<p class="text-xs text-muted-foreground uppercase">© 2024 BudgetBook Financial Architecture</p>
+		<nav class="flex items-center gap-4">
+			<a class="text-sm uppercase" href="/privacy-policy">Privacy Policy</a>
+			<a class="text-sm uppercase" href="/terms-of-service">Terms of Service</a>
+			<a class="text-sm uppercase" href="/help">Help</a>
+		</nav>
+	</footer>
 </div>
--- src/routes/(landing)/+page.svelte
+++ src/routes/(landing)/+page.svelte
@@ -1,1 +1,8 @@
-<h1>BudgetBook</h1>
+<section>
+	<div class="box flex flex-col items-center justify-center">
+		<h1>Master your personal finances.</h1>
+		<p>
+			The professional way to track, manage, and optimise your personal budget and saving goals.
+		</p>
+	</div>
+</section>
